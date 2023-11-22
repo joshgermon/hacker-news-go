62cea6774a21a9a17d8f01e38d23715825874db7
Joshua <joshgermon@gmail.com>
2023-11-22 19:30:16 +1100

    Get top stories and their titles

--- main.go
+++ main.go
@@ -2,41 +2,69 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-type Todo struct {
-	Id    int
-	Label string
+type HNItem struct {
+	By          string `json:"by,omitempty"`
+	Descendants int    `json:"descendants,omitempty"`
+	ID          int    `json:"id,omitempty"`
+	Kids        []int  `json:"kids,omitempty"`
+	Score       int    `json:"score,omitempty"`
+	Text        string `json:"text,omitempty"`
+	Time        int    `json:"time,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Type        string `json:"type,omitempty"`
 }
 
-
-func getTopStories() []int {
+func getTopStories() []HNItem {
     resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
     if err != nil {
         log.Fatal(err)
     }
     defer resp.Body.Close()
 
-    var topStories []int
-    err = json.NewDecoder(resp.Body).Decode(&topStories)
+    var topStoryIDs []int
+    err = json.NewDecoder(resp.Body).Decode(&topStoryIDs)
     if err != nil {
         log.Fatal(err)
     }
-    return topStories
+
+    var topStoryItems []HNItem
+    for _, id := range topStoryIDs {
+        topStoryItems = append(topStoryItems, getItem(id))
+    }
+
+    return topStoryItems
 }
 
-func getStory() {
+func getItem(id int) HNItem {
+    itemEndpoint := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", id)
+    resp, err := http.Get(itemEndpoint)
 
+    log.Printf("Calling endpoint %s", itemEndpoint)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+
+    var item HNItem
+
+    err = json.NewDecoder(resp.Body).Decode(&item)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return item
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-        getTopStories()
-		data := struct { TopStories []int }{ TopStories: getTopStories() }
+		data := getTopStories()
 		tmpl.Execute(w, data)
 	})
 
