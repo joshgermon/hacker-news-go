26173ff0539b8e936be54f51466edd05a813a6ba
Joshua <joshgermon@gmail.com>
2023-11-22 21:48:15 +1100

    Switch to algora api

--- main.go
+++ main.go
@@ -2,63 +2,139 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
+type HNSearchResult struct {
+	Hits         []HNItem `json:"hits"`
+	HitsPerPage  int      `json:"hitsPerPage"`
+	NbHits       int      `json:"nbHits"`
+	NbPages      int      `json:"nbPages"`
+	Page         int      `json:"page"`
+	Params       string   `json:"params"`
+	Query        string   `json:"query"`
+	ServerTimeMS int      `json:"serverTimeMS"`
+}
+
+type Author struct {
+	MatchLevel   string `json:"matchLevel"`
+	MatchedWords []any  `json:"matchedWords"`
+	Value        string `json:"value"`
+}
+
+type Title struct {
+	MatchLevel   string `json:"matchLevel"`
+	MatchedWords []any  `json:"matchedWords"`
+	Value        string `json:"value"`
+}
+
+type URL struct {
+	MatchLevel   string `json:"matchLevel"`
+	MatchedWords []any  `json:"matchedWords"`
+	Value        string `json:"value"`
+}
+
+type HighlightResult struct {
+	Author Author `json:"author"`
+	Title  Title  `json:"title"`
+	URL    URL    `json:"url"`
+}
+
+type StoryText struct {
+	MatchLevel   string `json:"matchLevel"`
+	MatchedWords []any  `json:"matchedWords"`
+	Value        string `json:"value"`
+}
+
 type HNItem struct {
-	By          string `json:"by,omitempty"`
-	Descendants int    `json:"descendants,omitempty"`
-	ID          int    `json:"id,omitempty"`
-	Kids        []int  `json:"kids,omitempty"`
-	Score       int    `json:"score,omitempty"`
-	Text        string `json:"text,omitempty"`
-	Time        int    `json:"time,omitempty"`
-	Title       string `json:"title,omitempty"`
-	Type        string `json:"type,omitempty"`
+	Tags        []string  `json:"_tags"`
+	Author      string    `json:"author"`
+	Children    []int     `json:"children"`
+	CreatedAt   time.Time `json:"created_at"`
+	CreatedAtI  int       `json:"created_at_i"`
+	NumComments int       `json:"num_comments"`
+	ObjectID    string    `json:"objectID"`
+	Points      int       `json:"points"`
+	StoryID     int       `json:"story_id"`
+	Title       string    `json:"title"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	URL         string    `json:"url"`
+	StoryText   string    `json:"story_text,omitempty"`
 }
 
-func getTopStories() []HNItem {
-    resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer resp.Body.Close()
-
-    var topStoryIDs []int
-    err = json.NewDecoder(resp.Body).Decode(&topStoryIDs)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    var topStoryItems []HNItem
-    for _, id := range topStoryIDs {
-        topStoryItems = append(topStoryItems, getItem(id))
-    }
-
-    return topStoryItems
+type HNStory struct {
+	Author          string `json:"author"`
+	CreatedAt       string `json:"created_at"`
+	NumComments     int    `json:"num_comments"`
+	Points          int    `json:"points"`
+	StoryID         int    `json:"story_id"`
+	Title           string `json:"title"`
+	LinkDescription string `json:"link_description"`
+	URL             string `json:"url"`
 }
 
-func getItem(id int) HNItem {
-    itemEndpoint := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", id)
-    resp, err := http.Get(itemEndpoint)
+func getLinkDescription(url string) (string, error) {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Println(err)
+	}
 
-    log.Printf("Calling endpoint %s", itemEndpoint)
-    if err != nil {
-        log.Fatal(err)
+    log.Printf("getting description for %s", url)
+
+	var description string
+	doc.Find("meta").Each(func(index int, item *goquery.Selection) {
+		if item.AttrOr("name", "") == "description" {
+			description = item.AttrOr("content", "")
+		}
+	})
+
+	return description, nil
+}
+
+func transformStoryData(story HNItem) HNStory {
+	layout := "January 2, 2006"
+	createdAt := story.CreatedAt.Format(layout)
+
+	linkDescription, err := getLinkDescription(story.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return HNStory{
+		Author:          story.Author,
+		CreatedAt:       createdAt,
+		NumComments:     story.NumComments,
+		Points:          story.Points,
+		StoryID:         story.StoryID,
+		Title:           story.Title,
+		LinkDescription: linkDescription,
+		URL:             story.URL,
+	}
+}
+
+func getTopStories() []HNStory {
+	resp, err := http.Get("https://hn.algolia.com/api/v1/search?tags=front_page")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var result HNSearchResult
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    var topStories []HNStory
+    for _, item := range result.Hits {
+        topStories = append(topStories, transformStoryData(item))
     }
-    defer resp.Body.Close()
 
-    var item HNItem
-
-    err = json.NewDecoder(resp.Body).Decode(&item)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    return item
+	return topStories
 }
 
 func main() {
@@ -70,4 +146,3 @@ func main() {
 
 	http.ListenAndServe(":8080", nil)
 }
-
